01101dc9d36b2e45e053e8fa869d95688c5b52f5
xiaoliang.ji <xiaoliang.ji@wormpex.com>
2021-03-15 19:29:02 +0800

    save

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
@@ -1,19 +1,29 @@
 package org.geektimes.projects.user.web.listener;
 
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.ConfigValue;
 import org.geektimes.context.ComponentContext;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.util.logging.Logger;
 
 @WebListener
 public class ComponentContextInitializerListener implements ServletContextListener {
-
+    Logger logger = Logger.getLogger(ComponentContextInitializerListener.class.getName());
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ComponentContext componentContext = new ComponentContext();
         componentContext.init(sce.getServletContext());
+
+        Config config = ConfigProvider.getConfig();
+
+        String appNameKey = "application.name";
+        String  appName = config.getValue(appNameKey, String.class);
+        logger.info("应用:"+appName +"初始化完成");
     }
 
     @Override
